35fc32167ba63ddba744715a2e618dd6f57617b6
Dhoka, Nikhil <ndhoka@iu.edu>
2024-01-20 17:13:55 -0500

    Added SQL Database without Redis

--- backend/DjangoDiabetesBackend/DjangoDiabetesBackend/settings.py
+++ backend/DjangoDiabetesBackend/DjangoDiabetesBackend/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = "DjangoDiabetesBackend.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydjangodiabetesdb',
-        'USER': 'raja',
-        'PASSWORD': '12345_Diabetes',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
