170d6bd4c2090177cb7bcdea6ec1ae192b45f298
RajaATAli <allmdararslan13@gmail.com>
2024-01-20 11:55:42 -0500

    Model registered in admin.py and SuperUser added

--- backend/DjangoDiabetesBackend/predictor/admin.py
+++ backend/DjangoDiabetesBackend/predictor/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from api.models import UserData
 
-# Register your models here.
+# Register your models here with 
+# Create Superuser with python manage.py createsuperuser
+admin.site.register(UserData)
+
